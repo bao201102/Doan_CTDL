5de454f73cbacd0e2e3568cbff51ee1b186d49ae
Lồ Sìn Dậu <luoshanyou75588@gmail.com>
2021-12-12 15:48:35 +0700

    Format code

---
+++ Car.cs
@@ -0,0 +1,33 @@
+namespace baithi
+{
+    public class Car
+    {
+        private string id;
+        private string name;
+        private float speed;
+        private float price;
+        public string getId()
+        {
+            return id;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public float getSpeed()
+        {
+            return speed;
+        }
+        public float getPrice()
+        {
+            return price;
+        }
+        public Car(string id, string name, float speed, float price)
+        {
+            this.id = id;
+            this.name = name;
+            this.speed = speed;
+            this.price = price;
+        }
+    }
+}
