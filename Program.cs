e93dd5f1df00df0806728a993d368cb57605d688
Nguyễn Ngọc Bảo <bao9d4@gmail.com>
2021-12-14 20:40:08 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace baithi
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Map bv_daihoc = new Map(50, 20, "Bệnh viện Đại học Y dược", "02040", "Quận 5", "TP.HCM"); //0
             Map ueh = new Map(40, 53.2f, "Đại học kinh tế TP.HCM CSB", "02241", "Quận 10", "TP.HCM"); //1
@@ -15,7 +16,7 @@ namespace baithi
             Map aeon = new Map(20.3f, 302.4f, "Trung tâm mua sắm AEON Mall Bình Tân", "132013", "Bình Tân", "TP.HCM"); //5
             Map khtn = new Map(49.2f, 90.2f, "Đại học Khoa học tự nhiên TPHCM", "360709", "Quận 8", "TP.HCM"); //6
             Map htv = new Map(20.4f, 30.2f, "Đài truyền hình HTV", "112394", "Quận 1", "TP.HCM"); //7
-        
+
             System.Console.WriteLine("-----------Các điểm xuất phát----------");
             System.Console.WriteLine("Bệnh viện đại học Y Dược: Phím 0");
             System.Console.WriteLine("Đại học kinh tế TP.HCM: Phím 1");
@@ -41,11 +42,13 @@ namespace baithi
                     map.AddVertex(khtn);
                     map.AddVertex(htv);
                     map.AddEdge(0, 1, 2); map.AddEdge(0, 3, 4); map.AddEdge(0, 4, 2);
-                    map.AddEdge(1, 2, 6); map.AddEdge(1, 4, 3); map.AddEdge(1, 6, 4);
-                    map.AddEdge(2, 5, 12); map.AddEdge(2, 6, 7); map.AddEdge(2, 7, 5);
-                    map.AddEdge(3, 5, 3); map.AddEdge(3, 6, 9);
-                    map.AddEdge(4, 5, 1); map.AddEdge(4, 7, 14);
-                    map.AddEdge(5, 6, 8); map.AddEdge(6, 7, 4);
+                    map.AddEdge(1, 2, 6); map.AddEdge(1, 4, 3); map.AddEdge(1, 6, 4); map.AddEdge(1, 0, 2);
+                    map.AddEdge(2, 5, 12); map.AddEdge(2, 6, 7); map.AddEdge(2, 7, 5); map.AddEdge(2, 1, 6);
+                    map.AddEdge(3, 5, 3); map.AddEdge(3, 6, 9); map.AddEdge(3, 0, 4);
+                    map.AddEdge(4, 5, 1); map.AddEdge(4, 7, 14); map.AddEdge(4, 1, 3); map.AddEdge(4, 0, 2);
+                    map.AddEdge(5, 6, 8); map.AddEdge(5, 2, 12); map.AddEdge(5, 3, 3); map.AddEdge(5, 4, 1);
+                    map.AddEdge(6, 7, 4); map.AddEdge(6, 1, 4); map.AddEdge(6, 2, 7); map.AddEdge(6, 3, 9); map.AddEdge(6, 5, 8);
+                    map.AddEdge(7, 2, 5); map.AddEdge(7, 4, 14); map.AddEdge(7, 6, 4);
                     System.Console.WriteLine("*****************************************************************");
                     System.Console.WriteLine("Điểm xuất phát:  " + bv_daihoc.getName());
                     System.Console.WriteLine("----------Các điểm đến------------");
@@ -104,12 +107,13 @@ namespace baithi
                     map1.AddVertex(khtn);
                     map1.AddVertex(htv);
                     map1.AddEdge(0, 1, 2); map1.AddEdge(0, 2, 6); map1.AddEdge(0, 4, 3); map1.AddEdge(0, 6, 4);
-                    map1.AddEdge(1, 3, 4); map1.AddEdge(1, 4, 2);
-                    map1.AddEdge(2, 0, 6); map1.AddEdge(2, 5, 12); map1.AddEdge(2, 6, 7); map1.AddEdge(2, 7, 5);
-                    map1.AddEdge(3, 5, 3); map1.AddEdge(3, 6, 9);
-                    map1.AddEdge(4, 5, 1); map1.AddEdge(4, 7, 14);
-                    map1.AddEdge(5, 6, 8);
-                    map1.AddEdge(6, 7, 4);
+                    map1.AddEdge(1, 3, 4); map1.AddEdge(1, 4, 2); map1.AddEdge(1, 0, 2);
+                    map1.AddEdge(2, 5, 12); map1.AddEdge(2, 6, 7); map1.AddEdge(2, 7, 5); map1.AddEdge(2, 0, 6);
+                    map1.AddEdge(3, 5, 3); map1.AddEdge(3, 6, 9); map1.AddEdge(3, 1, 4);
+                    map1.AddEdge(4, 5, 1); map1.AddEdge(4, 7, 14); map1.AddEdge(4, 1, 2); map1.AddEdge(4, 0, 3);
+                    map1.AddEdge(5, 6, 8); map1.AddEdge(5, 2, 12); map1.AddEdge(5, 3, 3); map1.AddEdge(5, 4, 1);
+                    map1.AddEdge(6, 7, 4); map1.AddEdge(6, 2, 7); map1.AddEdge(6, 3, 9); map1.AddEdge(6, 5, 8); map1.AddEdge(6, 0, 4);
+                    map1.AddEdge(7, 2, 5); map1.AddEdge(7, 4, 14); map1.AddEdge(7, 6, 4);
                     System.Console.WriteLine("*****************************************************************");
                     System.Console.WriteLine("Điểm xuất phát: " + ueh.getName());
                     System.Console.WriteLine("----------Các điểm đến------------");
@@ -161,15 +165,16 @@ namespace baithi
                     Graph map2 = new Graph();
                     map2.AddVertex(chobenthanh); map2.AddVertex(ueh);
                     map2.AddVertex(bv_daihoc); map2.AddVertex(bv_hungvuong);
-                    map2.AddVertex(macdinhchi);  map2.AddVertex(aeon);
+                    map2.AddVertex(macdinhchi); map2.AddVertex(aeon);
                     map2.AddVertex(khtn); map2.AddVertex(htv);
                     map2.AddEdge(0, 1, 6); map2.AddEdge(0, 5, 12); map2.AddEdge(0, 6, 7); map2.AddEdge(0, 7, 5);
                     map2.AddEdge(1, 2, 2); map2.AddEdge(1, 4, 3); map2.AddEdge(1, 6, 4); map2.AddEdge(1, 0, 6);
-                    map2.AddEdge(2, 3, 4); map2.AddEdge(2, 4, 2);
-                    map2.AddEdge(3, 5, 3); map2.AddEdge(3, 6, 9);
-                    map2.AddEdge(4, 5, 1); map2.AddEdge(4, 7, 14);
-                    map2.AddEdge(5, 6, 8);
-                    map2.AddEdge(6, 7, 4);
+                    map2.AddEdge(2, 3, 4); map2.AddEdge(2, 4, 2); map2.AddEdge(2, 1, 2);
+                    map2.AddEdge(3, 5, 3); map2.AddEdge(3, 6, 9); map2.AddEdge(3, 2, 4);
+                    map2.AddEdge(4, 5, 1); map2.AddEdge(4, 7, 14); map2.AddEdge(4, 1, 3); map2.AddEdge(4, 2, 2);
+                    map2.AddEdge(5, 6, 8); map2.AddEdge(5, 3, 3); map2.AddEdge(5, 4, 1); map2.AddEdge(5, 0, 12);
+                    map2.AddEdge(6, 7, 4); map2.AddEdge(6, 1, 4); map2.AddEdge(6, 3, 9); map2.AddEdge(6, 5, 8); map2.AddEdge(6, 0, 7);
+                    map2.AddEdge(7, 4, 14); map2.AddEdge(7, 6, 4); map2.AddEdge(7, 0, 5);
                     System.Console.WriteLine("*****************************************************************");
                     System.Console.WriteLine("Điểm xuất phát: " + chobenthanh.getName());
                     System.Console.WriteLine("----------Các điểm đến------------");
@@ -221,15 +226,16 @@ namespace baithi
                     Graph map3 = new Graph();
                     map3.AddVertex(bv_hungvuong); map3.AddVertex(bv_daihoc);
                     map3.AddVertex(ueh); map3.AddVertex(chobenthanh);
-                    map3.AddVertex(macdinhchi);  map3.AddVertex(aeon);
+                    map3.AddVertex(macdinhchi); map3.AddVertex(aeon);
                     map3.AddVertex(khtn); map3.AddVertex(htv);
                     map3.AddEdge(0, 1, 4); map3.AddEdge(0, 5, 3); map3.AddEdge(0, 6, 9);
-                    map3.AddEdge(1, 4, 2); map3.AddEdge(1, 2, 2);
-                    map3.AddEdge(2, 4, 3); map3.AddEdge(2, 6, 4); map3.AddEdge(2, 3, 6);
-                    map3.AddEdge(3, 5, 12); map3.AddEdge(3, 6, 7); map3.AddEdge(3, 7, 4);
-                    map3.AddEdge(4, 5, 1); map3.AddEdge(4, 7, 14);
-                    map3.AddEdge(5, 6, 8);
-                    map3.AddEdge(6, 7, 4);
+                    map3.AddEdge(1, 4, 2); map3.AddEdge(1, 2, 2); map3.AddEdge(1, 0, 4);
+                    map3.AddEdge(2, 4, 3); map3.AddEdge(2, 6, 4); map3.AddEdge(2, 3, 6); map3.AddEdge(2, 1, 2);
+                    map3.AddEdge(3, 5, 12); map3.AddEdge(3, 6, 7); map3.AddEdge(3, 7, 4); map3.AddEdge(3, 2, 6);
+                    map3.AddEdge(4, 5, 1); map3.AddEdge(4, 7, 14); map3.AddEdge(4, 1, 2); map3.AddEdge(4, 2, 3);
+                    map3.AddEdge(5, 6, 8); map3.AddEdge(5, 3, 12); map3.AddEdge(5, 4, 1); map3.AddEdge(5, 0, 3);
+                    map3.AddEdge(6, 7, 4); map3.AddEdge(6, 2, 4); map3.AddEdge(6, 3, 7); map3.AddEdge(6, 5, 8); map3.AddEdge(6, 0, 9);
+                    map3.AddEdge(7, 3, 4); map3.AddEdge(7, 4, 14); map3.AddEdge(7, 6, 4);
                     System.Console.WriteLine("*****************************************************************");
                     System.Console.WriteLine("Điểm xuất phát: " + bv_hungvuong.getName());
                     System.Console.WriteLine("----------Các điểm đến------------");
@@ -279,17 +285,18 @@ namespace baithi
                     break;
                 case 4:
                     Graph map4 = new Graph();
-                    map4.AddVertex(macdinhchi); map4.AddVertex(bv_daihoc); 
+                    map4.AddVertex(macdinhchi); map4.AddVertex(bv_daihoc);
                     map4.AddVertex(ueh); map4.AddVertex(chobenthanh);
                     map4.AddVertex(bv_hungvuong); map4.AddVertex(aeon);
                     map4.AddVertex(khtn); map4.AddVertex(htv);
                     map4.AddEdge(0, 1, 2); map4.AddEdge(0, 2, 3); map4.AddEdge(0, 5, 1); map4.AddEdge(0, 7, 14);
-                    map4.AddEdge(1, 4, 4); map4.AddEdge(1, 2, 2);
-                    map4.AddEdge(2, 6, 4); map4.AddEdge(2, 3, 6);
-                    map4.AddEdge(3, 5, 12); map4.AddEdge(3, 6, 7); map4.AddEdge(3, 7, 5);
-                    map4.AddEdge(4, 5, 3); map4.AddEdge(4, 6, 9);
-                    map4.AddEdge(5, 6, 8);
-                    map4.AddEdge(6, 7, 4);
+                    map4.AddEdge(1, 4, 4); map4.AddEdge(1, 2, 2); map4.AddEdge(1, 0, 2);
+                    map4.AddEdge(2, 6, 4); map4.AddEdge(2, 3, 6); map4.AddEdge(2, 1, 2); map4.AddEdge(2, 0, 3);
+                    map4.AddEdge(3, 5, 12); map4.AddEdge(3, 6, 7); map4.AddEdge(3, 7, 5); map4.AddEdge(3, 2, 6);
+                    map4.AddEdge(4, 5, 3); map4.AddEdge(4, 6, 9); map4.AddEdge(4, 1, 4);
+                    map4.AddEdge(5, 6, 8); map4.AddEdge(5, 3, 12); map4.AddEdge(5, 4, 3); map4.AddEdge(5, 0, 1);
+                    map4.AddEdge(6, 7, 4); map4.AddEdge(6, 2, 4); map4.AddEdge(6, 3, 7); map4.AddEdge(6, 4, 9); map4.AddEdge(6, 5, 8);
+                    map4.AddEdge(7, 3, 5); map4.AddEdge(7, 6, 4); map4.AddEdge(7, 0, 14);
                     System.Console.WriteLine("*****************************************************************");
                     System.Console.WriteLine("Điểm xuất phát: " + macdinhchi.getName());
                     System.Console.WriteLine("----------Các điểm đến------------");
@@ -343,13 +350,14 @@ namespace baithi
                     map5.AddVertex(bv_daihoc); map5.AddVertex(ueh);
                     map5.AddVertex(macdinhchi); map5.AddVertex(chobenthanh);
                     map5.AddVertex(khtn); map5.AddVertex(htv);
-                    map5.AddEdge(0, 1, 3); map5.AddEdge(0, 4, 1); map5.AddEdge(0, 6, 8);
-                    map5.AddEdge(1, 2, 4); map5.AddEdge(1, 6, 9);
-                    map5.AddEdge(2, 4, 2); map5.AddEdge(2, 3, 2);
-                    map5.AddEdge(3, 4, 3); map5.AddEdge(3, 6, 4); map5.AddEdge(3, 5, 6);
-                    map5.AddEdge(4, 7, 14);
-                    map5.AddEdge(5, 6, 7); map5.AddEdge(5, 7, 5);
-                    map5.AddEdge(6, 7, 4);
+                    map5.AddEdge(0, 1, 3); map5.AddEdge(0, 4, 1); map5.AddEdge(0, 6, 8); map5.AddEdge(0, 5, 2);
+                    map5.AddEdge(1, 2, 4); map5.AddEdge(1, 6, 9); map5.AddEdge(1, 0, 3);
+                    map5.AddEdge(2, 4, 2); map5.AddEdge(2, 3, 2); map5.AddEdge(2, 1, 4);
+                    map5.AddEdge(3, 4, 3); map5.AddEdge(3, 6, 4); map5.AddEdge(3, 5, 6); map5.AddEdge(3, 2, 2);
+                    map5.AddEdge(4, 7, 14); map5.AddEdge(4, 2, 2); map5.AddEdge(4, 3, 3); map5.AddEdge(4, 0, 1);
+                    map5.AddEdge(5, 6, 7); map5.AddEdge(5, 7, 5); map5.AddEdge(5, 3, 6); map5.AddEdge(5, 0, 2);
+                    map5.AddEdge(6, 7, 4); map5.AddEdge(6, 1, 9); map5.AddEdge(6, 3, 4); map5.AddEdge(6, 5, 7); map5.AddEdge(6, 0, 8);
+                    map5.AddEdge(7, 4, 14); map5.AddEdge(7, 5, 5); map5.AddEdge(7, 6, 4);
                     System.Console.WriteLine("*****************************************************************");
                     System.Console.WriteLine("Điểm xuất phát: " + aeon.getName());
                     System.Console.WriteLine("----------Các điểm đến------------");
@@ -397,19 +405,20 @@ namespace baithi
                             break;
                     }
                     break;
-                case 6: 
+                case 6:
                     Graph map6 = new Graph();
                     map6.AddVertex(khtn); map6.AddVertex(htv);
                     map6.AddVertex(chobenthanh); map6.AddVertex(ueh);
                     map6.AddVertex(macdinhchi); map6.AddVertex(aeon);
                     map6.AddVertex(bv_hungvuong); map6.AddVertex(bv_daihoc);
                     map6.AddEdge(0, 1, 4); map6.AddEdge(0, 2, 7); map6.AddEdge(0, 3, 4); map6.AddEdge(0, 5, 8); map6.AddEdge(0, 6, 9);
-                    map6.AddEdge(1, 4, 14); map6.AddEdge(1, 2, 5);
-                    map6.AddEdge(2, 3, 6); map6.AddEdge(2, 5, 12);
-                    map6.AddEdge(3, 4, 3); map6.AddEdge(3, 7, 2);
-                    map6.AddEdge(4, 7, 2); map6.AddEdge(4, 5, 1);
-                    map6.AddEdge(5, 6, 3);
-                    map6.AddEdge(6, 7, 4);
+                    map6.AddEdge(1, 4, 14); map6.AddEdge(1, 2, 5); map6.AddEdge(1, 0, 4);
+                    map6.AddEdge(2, 3, 6); map6.AddEdge(2, 5, 12); map6.AddEdge(2, 1, 5); map6.AddEdge(2, 0, 7);
+                    map6.AddEdge(3, 4, 3); map6.AddEdge(3, 7, 2); map6.AddEdge(3, 2, 6); map6.AddEdge(3, 0, 4);
+                    map6.AddEdge(4, 7, 2); map6.AddEdge(4, 5, 1); map6.AddEdge(4, 1, 14); map6.AddEdge(4, 3, 3);
+                    map6.AddEdge(5, 6, 3); map6.AddEdge(5, 2, 12); map6.AddEdge(5, 4, 1); map6.AddEdge(5, 0, 8);
+                    map6.AddEdge(6, 7, 4); map6.AddEdge(6, 5, 3); map6.AddEdge(6, 0, 9);
+                    map6.AddEdge(7, 3, 2); map6.AddEdge(7, 4, 2); map6.AddEdge(7, 6, 4);
                     System.Console.WriteLine("*****************************************************************");
                     System.Console.WriteLine("Điểm xuất phát: " + khtn.getName());
                     System.Console.WriteLine("----------Các điểm đến------------");
@@ -464,12 +473,13 @@ namespace baithi
                     map7.AddVertex(macdinhchi); map7.AddVertex(aeon);
                     map7.AddVertex(bv_hungvuong); map7.AddVertex(bv_daihoc);
                     map7.AddEdge(0, 1, 4); map7.AddEdge(0, 4, 14); map7.AddEdge(0, 2, 5);
-                    map7.AddEdge(1, 2, 7); map7.AddEdge(1, 3, 4); map7.AddEdge(1, 5, 8); map7.AddEdge(1, 6, 9);
-                    map7.AddEdge(2, 5, 12); map7.AddEdge(2, 3, 6);
-                    map7.AddEdge(3, 4, 3); map7.AddEdge(3, 7, 2);
-                    map7.AddEdge(4, 7, 2); map7.AddEdge(4, 5, 1);
-                    map7.AddEdge(5, 6, 3);
-                    map7.AddEdge(6, 7, 4);
+                    map7.AddEdge(1, 2, 7); map7.AddEdge(1, 3, 4); map7.AddEdge(1, 5, 8); map7.AddEdge(1, 6, 9); map7.AddEdge(1, 0, 4);
+                    map7.AddEdge(2, 5, 12); map7.AddEdge(2, 3, 6); map7.AddEdge(2, 1, 7); map7.AddEdge(2, 0, 5);
+                    map7.AddEdge(3, 4, 3); map7.AddEdge(3, 7, 2); map7.AddEdge(3, 1, 4); map7.AddEdge(3, 2, 6);
+                    map7.AddEdge(4, 7, 2); map7.AddEdge(4, 5, 1); map7.AddEdge(4, 3, 3); map7.AddEdge(4, 0, 14);
+                    map7.AddEdge(5, 6, 3); map7.AddEdge(5, 1, 8); map7.AddEdge(5, 2, 12); map7.AddEdge(5, 4, 1);
+                    map7.AddEdge(6, 7, 4); map7.AddEdge(6, 1, 9); map7.AddEdge(6, 5, 3);
+                    map7.AddEdge(7, 3, 2); map7.AddEdge(7, 4, 2); map7.AddEdge(7, 6, 4);
                     System.Console.WriteLine("*****************************************************************");
                     System.Console.WriteLine("Điểm xuất phát: " + htv.getName());
                     System.Console.WriteLine("----------Các điểm đến------------");
